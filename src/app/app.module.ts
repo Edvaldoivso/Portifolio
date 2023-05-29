136261a790658bd59f1a99488eb947bc6434a6be
Edvaldo <49847797+Edvaldoivso@users.noreply.github.com>
2023-05-29 01:10:09 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './Footer/footer.component';
+import { HeaderComponent } from './Head/header.component';
+import { HomeComponent } from './Home/home.component';
+import { MenuComponent } from './Menu/menu.component';
+import { SponsorshipComponent } from './Sponsorship/sponsorship.component';
+import { BodyComponent } from './body/body.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    MenuComponent,
+    SponsorshipComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
