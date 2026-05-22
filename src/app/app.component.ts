ee244fb47f10a24be631ae339585d3d6a2686af4
Edvaldo <49847797+Edvaldoivso@users.noreply.github.com>
2026-05-21 21:09:41 -0300

    Adcionado os detalhes finais pronto para ativar os textos fixos dos manuais para a pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+
+  ngOnInit(): void {
+
+    document.addEventListener('contextmenu', (event) => {
+      event.preventDefault();
+    });
+
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+
+    // CTRL + C
+    if (event.ctrlKey && event.key.toLowerCase() === 'c') {
+      event.preventDefault();
+    }
+
+    // CTRL + U
+    if (event.ctrlKey && event.key.toLowerCase() === 'u') {
+      event.preventDefault();
+    }
+
+    // F12
+    if (event.key === 'F12') {
+      event.preventDefault();
+    }
+
+  }
+
+}
